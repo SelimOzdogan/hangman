8e35880d78e728b1cc307df1d9f38d29260d7df2
Selim Ozdogan <selimozdogan@hotmail.com>
2020-05-28 23:28:55 -0700

    Cleared Letters

--- js/main.js
+++ js/main.js
@@ -12,27 +12,20 @@ $(document).ready(function () {
 
 
 window.addEventListener('DOMContentLoaded', (e) => {
-    createLetters($("#letters_div"));
-    // $("#letter_A").click(function(){
-    //         alert("");
-
-    // });
-    $("#letters_div").find("div").map(function () {
-        $("#" + this.id).click(function () {
-            console.log(this.innerHTML);
-            this.classList.add("selected");
-        })
-    });
+    createLetters();
+    addClickToButtons();
 });
 
-function createLetters(letters_ul) {
+function createLetters() {
+    let letters_div = $("#letters_div");
+
     let letter = "A"
-    letters_ul.append(createLetter(letter))
+    letters_div.append(createLetter(letter))
     do {
         letter = String.fromCharCode(letter.charCodeAt() + 1)
-        letters_ul.append(createLetter(letter))
+        letters_div.append(createLetter(letter))
     } while (letter != "Z")
-    return letters_ul;
+    addClickEvents(letters_div);
 }
 function createLetter(letter) {
     const letter_div = document.createElement('div')
@@ -41,7 +34,24 @@ function createLetter(letter) {
     letter_div.innerHTML = letter;
     return letter_div;
 }
-
-function checkLetter(){
+function addClickEvents(letters_div) {
+    letters_div.find("div").map(function () {
+        $("#" + this.id).click(function () {
+            console.log(this.innerHTML);
+            this.classList.add("selected");
+        })
+    });
+}
+function addClickToButtons() {
+    const button = $("#newGame");
+    button.click(function () {
+        clearAll();
+    });
+}
+function clearAll() {
+    $("#letters_div").find("div").removeClass("selected");
+    createQuestion();
+}
+function checkLetter() {
 
 }
