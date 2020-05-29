1f7a6f5f805412bf207df881052c29349f2bc7e4
Selim Ozdogan <selimozdogan@hotmail.com>
2020-05-29 01:41:23 -0700

    Added gallows

--- js/main.js
+++ js/main.js
@@ -20,25 +20,35 @@ function createLetters() {
     let letters_div = $("#letters_div");
 
     let letter = "A"
-    letters_div.append(createLetter(letter))
+    letters_div.append(createDiv("letter", letter, letter))
     do {
         letter = String.fromCharCode(letter.charCodeAt() + 1)
-        letters_div.append(createLetter(letter))
+        letters_div.append(createDiv("letter", letter, letter))
     } while (letter != "Z")
     addClickEvents(letters_div);
 }
-function createLetter(letter) {
-    const letter_div = document.createElement('div')
-    letter_div.id = 'letter_' + letter;
-    letter_div.classList.add('letter_div')
-    letter_div.innerHTML = letter;
-    return letter_div;
+function createDiv(prefixForId, suffixForId, text) {
+    const div = document.createElement('div')
+    div.id = prefixForId + '_' + suffixForId;
+    div.classList.add(prefixForId + '_div')
+    div.innerHTML = text;
+    return div;
+}
+function createQuestion() {
+    let question_letters = $("#question_letters_div");
+    question_letters.empty();
+    for (let i = 1; i <= getQuestion().length; i += 1) {
+
+        question_letters.append(createDiv("question_letter", i, "&nbsp;&nbsp;"));
+    }
 }
 function addClickEvents(letters_div) {
     letters_div.find("div").map(function () {
         $("#" + this.id).click(function () {
+
             console.log(this.innerHTML);
             this.classList.add("selected");
+            checkLetter(this.innerHTML)
         })
     });
 }
@@ -50,8 +60,26 @@ function addClickToButtons() {
 }
 function clearAll() {
     $("#letters_div").find("div").removeClass("selected");
+    $("#gallows").attr.removeClass("selected");
     createQuestion();
 }
-function checkLetter() {
 
+function getQuestion() {
+    return "Selim".toUpperCase();
+}
+
+function checkLetter(letter) {
+    let q = getQuestion();
+    if (q.includes(letter)) {
+        q.split("").map(function (c, i) {
+            if (c == letter) {
+                $("#question_letter_" + (i + 1))[0].innerHTML = letter;
+                
+            }
+        })
+    }
+    else {
+
+    }
+    checkIsitFinished();
 }
