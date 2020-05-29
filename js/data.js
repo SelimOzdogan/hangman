246502dff602151984bc3039d96c783b7e605e1d
Selim Ozdogan <selimozdogan@hotmail.com>
2020-05-29 03:01:55 -0700

    Added Names

---
+++ js/data.js
@@ -0,0 +1,5 @@
+CaData = ["Oliver", "Liam", "Lucas", "Ethan", "Noah", "William", "Benjamin", "Theodore",
+    "Leo", "Logan", "Henry", "Owen", "Jack", "Jacob", "James", "Jackson", "Hudson",
+    "Levi", "Alexander", "Daniel", "Olivia", "Charlotte", "Emma", "Ava",
+    "Sophia", "Amelia", "Chloe", "Mia", "Isla", "Evelyn", "Ella", "Emily",
+    "Hannah", "Avery", "Lily", "Mila", "Abigail", "Isabella", "Elizabeth", "Aria"]
