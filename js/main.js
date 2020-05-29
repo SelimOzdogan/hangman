9cd9c28c018650a0d08e0f1eb366acaf50016b10
Selim Ozdogan <selimozdogan@hotmail.com>
2020-05-28 21:30:02 -0700

    Created Letters

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
 // These are some event handlers to make the demo work well.
 // Don't change them unless you know what you're doing!
-$(document).ready(function() {
-	$('#reset').click(function() {
-		document.location.reload();
-	});
+$(document).ready(function () {
+    $('#reset').click(function () {
+        document.location.reload();
+    });
 
-	$('form').submit(function(event) {
-		event.preventDefault();
-	});
+    $('form').submit(function (event) {
+        event.preventDefault();
+    });
 });
 
-    
+
+window.addEventListener('DOMContentLoaded', (e) => {
+    createLetters($("#letters_div"));
+});
+
+function createLetters(letters_ul) {
+    let letter = "A"
+    letters_ul.append(createLetter(letter))
+    do {
+        letter = String.fromCharCode(letter.charCodeAt() + 1)
+        letters_ul.append(createLetter(letter))
+    } while (letter != "Z")
+    return letters_ul;
+}
+function createLetter(letter) {
+    const letter_div = document.createElement('div')
+    letter_div.id = 'letter_' + letter;
+    letter_div.classList.add('letter_div')
+    letter_div.innerHTML = letter;
+    letter_div.click(function () {
+        alert( "Handler for .click() called." );
+    });
+    return letter_div;
+}
