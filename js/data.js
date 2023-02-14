b77ca10c852609cf4351bc5adc7bbcfd93a76cf5
Selim <53090714+SelimOzdogan@users.noreply.github.com>
2023-02-14 15:30:14 -0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,5 @@
- let CaData = ["selim"];
+ let CaData = ["Oliver", "Liam", "Lucas", "Ethan", "Noah", "William", "Benjamin", "Theodore",
+    "Leo", "Logan", "Henry", "Owen", "Jack", "Jacob", "James", "Jackson", "Hudson",
+    "Levi", "Alexander", "Daniel", "Olivia", "Charlotte", "Emma", "Ava",
+    "Sophia", "Amelia", "Chloe", "Mia", "Isla", "Evelyn", "Ella", "Emily",
+    "Hannah", "Avery", "Lily", "Mila", "Abigail", "Isabella", "Elizabeth", "Aria"];
